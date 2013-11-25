524bb726358b50d51513c38f707bfc189bd6b8ba
Simon Colmer <simon_colmer@hotmail.com>
2013-11-25 18:33:26 +0000

    Updated default values

--- js/systemParameters.js
+++ js/systemParameters.js
@@ -2,10 +2,10 @@ function SystemParameters(utils) {
 	var debug = utils.urlParam("debug") != 0,
 	 	indicator = debug ? {fill: 'white', scale: 3} : null,
 	 	defaultSpeed = utils.urlParam("speed") || 2,
-	 	bees = utils.urlParam("bees") || 64,
+	 	bees = utils.urlParam("bees") || 100,
 	 	beeColour = '#00FF33',
 	 	beeAltColour = '#20B2AA',
-	 	targets = utils.urlParam("targets") || 2;
+	 	targets = utils.urlParam("targets") || 3;
 
  	return {
  		debug: debug,
