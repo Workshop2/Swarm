641d8f51e0e6a481b719e48ea2e05ead1f791775
Simon Colmer <simon_colmer@hotmail.com>
2013-11-25 18:38:30 +0000

    Reducing bee count by default

--- js/systemParameters.js
+++ js/systemParameters.js
@@ -2,7 +2,7 @@ function SystemParameters(utils) {
 	var debug = utils.urlParam("debug") != 0,
 	 	indicator = debug ? {fill: 'white', scale: 3} : null,
 	 	defaultSpeed = utils.urlParam("speed") || 1,
-	 	bees = utils.urlParam("bees") || 100,
+	 	bees = utils.urlParam("bees") || 64,
 	 	beeColour = '#00FF33',
 	 	beeAltColour = '#20B2AA',
 	 	targets = utils.urlParam("targets") || 3;
