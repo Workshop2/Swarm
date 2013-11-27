4383506510965ef3d8991246767a11670f0f9c7f
Simon Colmer <simon_colmer@hotmail.com>
2013-11-27 18:27:13 +0000

    Refactoring. Group bees now lives in targetCollection

--- js/beeCollection.js
+++ js/beeCollection.js
@@ -10,12 +10,12 @@ function BeeCollection(two, playArea, utils, systemParameters) {
 	};
 
 	var update = function(targetCollection) {
-		var targetGroups = groupBees(targetCollection);
+		var targetGroups = targetCollection.groupBees(bees);
 
 		for (var x = targetGroups.length - 1; x >= 0; x--) {
 			var targetGroup = targetGroups[x];
 
-			// get closest bee
+			// find closest bee to target e.g. the leader
 			var leader = null;
 			for (var i = targetGroup.bees.length - 1; i >= 0; i--) {
 				var bee = targetGroup.bees[i],
@@ -36,53 +36,11 @@ function BeeCollection(two, playArea, utils, systemParameters) {
 
 				if(bee == leader.bee) {
 					bee.update(targetGroup.target, systemParameters.indicator);
-				}
-				else		
+				} else {
 					bee.update(leader.bee.dot);
-			};
-		};
-	};
-
-	// will return a list of grouped targets and children
-	var groupBees = function(targetCollection) {
-		var targetGroups = [];
-
-		// turn target collection into a wrapped target with children
-		for (var i = targetCollection.length - 1; i >= 0; i--) {
-			targetGroups.push({ target: targetCollection[i], bees: [] });
-		};
-
-		// assign a bee to a target group
-		for (var i = bees.length - 1; i >= 0; i--) {
-			var bee = bees[i];
-			var evalTargets = [];
-
-			// builds a list of distances between all available targets
-			for (var j = targetCollection.length - 1; j >= 0; j--) {
-				var target = targetCollection[j],
-					distanceTo = utils.distanceTo(bee.dot.translation, target.translation);
-
-				evalTargets.push({target: target, distance: utils.getDistance(distanceTo) })
-			};
-
-			var beeTarget = null;
-			for (var k = evalTargets.length - 1; k >= 0; k--) {
-				var evalTarget = evalTargets[k];
-				if(beeTarget == null || evalTarget.distance < beeTarget.distance) {
-					beeTarget = evalTarget;
-				}
-			};
-
-			for (var l = targetGroups.length - 1; l >= 0; l--) {
-				var targetGroup = targetGroups[l];
-				if(targetGroup.target == beeTarget.target) {
-					targetGroup.bees.push(bee);
-					break;
 				}
 			};
 		};
-
-		return targetGroups;
 	};
 
 	return {
--- js/targetCollection.js
+++ js/targetCollection.js
@@ -14,9 +14,51 @@ function TargetCollection(two, playArea, utils, systemParameters) {
 		};
 	};
 
+	var groupBees = function(bees) {
+		var targetGroups = [];
+
+		// turn target collection into a wrapped target with children
+		for (var i = targets.length - 1; i >= 0; i--) {
+			targetGroups.push({ target: targets[i], bees: [] });
+		};
+
+		// assign a bee to a target group
+		for (var i = bees.length - 1; i >= 0; i--) {
+			var bee = bees[i];
+			var evalTargets = [];
+
+			// builds a list of distances between all available targets
+			for (var j = targets.length - 1; j >= 0; j--) {
+				var target = targets[j],
+					distanceTo = utils.distanceTo(bee.dot.translation, target.translation);
+
+				evalTargets.push({target: target, distance: utils.getDistance(distanceTo) })
+			};
+
+			var beeTarget = null;
+			for (var k = evalTargets.length - 1; k >= 0; k--) {
+				var evalTarget = evalTargets[k];
+				if(beeTarget == null || evalTarget.distance < beeTarget.distance) {
+					beeTarget = evalTarget;
+				}
+			};
+
+			for (var l = targetGroups.length - 1; l >= 0; l--) {
+				var targetGroup = targetGroups[l];
+				if(targetGroup.target == beeTarget.target) {
+					targetGroup.bees.push(bee);
+					break;
+				}
+			};
+		};
+
+		return targetGroups;
+	};
+
 	return {
 		update: update,
 		spawnTarget: spawnTarget,
-		targets: targets
+		targets: targets,
+		groupBees: groupBees
 	};
 }
